97f9dbf1d6e3b8248a7e7f62625ccfb7cf45568b
MinheeDK <minheedk@hotmail.com>
2021-04-30 17:04:51 +0200

    Oprettet integrationstest 4 - CookController_Display samt lavet rettelser i Timer.cs og TimerTest.cs

---
+++ MicrowaveOven.Test.Integration/IT4__CookController_Display.cs
@@ -0,0 +1,58 @@
+﻿using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MicrowaveOven.Test.Integration
+{
+    [TestFixture]
+    public class IT4__CookController_Display
+    {
+        //Testdriver
+        public CookController _ck;
+
+        //Egentlige klasser
+        private Display _display;
+        private Output _output;
+
+        //Fakes
+        private IPowerTube _powerTube;
+        private ITimer _timer;
+        private IUserInterface _userInterface;
+
+        //StringWriter
+        private StringWriter _sW;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+
+            _output = new Output();
+            _display = new Display(_output);
+
+            _powerTube = Substitute.For<IPowerTube>();
+            _timer = Substitute.For<ITimer>();
+            _userInterface = Substitute.For<IUserInterface>();
+            _ck = new CookController(_timer, _display, _powerTube);
+            _sW = new StringWriter();
+            Console.SetOut(_sW);
+        }
+
+        [TestCase("00","30")]
+        [TestCase("01", "22")]
+        [TestCase("02", "00")]
+        public void TimeRemaining_On_Display_In_MinutesAndSeconds(string min, string sec)
+        {
+            _timer.TimeRemaining.Returns(Convert.ToInt32(min) * 60 + Convert.ToInt32(sec));
+            _timer.TimerTick += Raise.EventWith(this, EventArgs.Empty);
+
+            Assert.That(_sW.ToString().Contains(min+":"+sec));
+        }
+    }
+}
