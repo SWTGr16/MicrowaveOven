02842d258d2496521fde4b8c3dbfaf25eb73ef98
KerstinFroberg <au639158@post.au.dk>
2021-05-05 09:21:09 +0200

    Step 1, 2 og 3 test opdateret.

--- MicrowaveOven.Test.Integration/IT01_Light.cs
+++ MicrowaveOven.Test.Integration/IT01_Light.cs
@@ -7,6 +7,10 @@ namespace MicrowaveOven.Test.Integration
     [TestFixture]
     public class IT01_Light
     {
+        //Integrationtest 01:
+        //Light = T
+        //Output = X
+
         private Light _light;
         private Output _output;
         public StringWriter _stringWriter;
@@ -15,21 +19,25 @@ namespace MicrowaveOven.Test.Integration
         public void Setup()
         {
             _output = new Output();
-            _light = new Light(_output); //Constructor har én parameter
+            _light = new Light(_output); //Constructor tager én parameter
             _stringWriter = new StringWriter();
+            System.Console.SetOut(_stringWriter);
         }
 
         [Test]
-        public void isOn_isTrue_TurnOn_Output_LightIsTurnedOn()
+        public void TurnOn_Output_LightIsTurnedOn()
         {
             _light.TurnOn();
-            Assert.That(_stringWriter.ToString().Equals("Light is turned on"));
+            Assert.That(_stringWriter.ToString().Contains("turned on"));
         }
 
-        public void isOn_isFalse_TurnOff_Output_LightIsTurnedOff()
+        [Test]
+        public void TurnOff_Output_LightIsTurnedOff()
         {
+            _light.TurnOn(); 
             _light.TurnOff();
-            Assert.That(_stringWriter.ToString().Equals("Light is turned off"));
+
+            Assert.That(_stringWriter.ToString().Contains("turned off"));
         }
     }
 }
--- MicrowaveOven.Test.Integration/IT03_PowerTube.cs
+++ MicrowaveOven.Test.Integration/IT03_PowerTube.cs
@@ -1,18 +1,45 @@
+using System;
+using System.IO;
+using Microwave.Classes.Boundary;
 using NUnit.Framework;
 
 namespace MicrowaveOven.Test.Integration
 {
-    public class Tests
+    [TestFixture]
+    public class IT03_PowerTube
     {
+        //Integrationstest 03:
+        //PowerTube = T
+        //Output = X
+
+        private Output _output;
+        private PowerTube _powerTube;
+        private StringWriter _stringWriter;
+
         [SetUp]
         public void Setup()
         {
+            _output = new Output();
+            _powerTube = new PowerTube(_output);
+            _stringWriter = new StringWriter();
         }
 
         [Test]
-        public void Test1()
+        public void TurnOff_OutputLine_displays_PowerTubeTurnedOff()
         {
-            Assert.Pass();
+            _powerTube.TurnOn(60);
+            _powerTube.TurnOff();
+            Assert.That(!_stringWriter.ToString().Contains("off"));
+        }
+
+        [TestCase(50)]
+        [TestCase(100)]
+        [TestCase(600)]
+        [TestCase(700)]
+        public void TurnOn_OutputLine_displays_PowerTubeWorksWithPower(int power)
+        {
+            _powerTube.TurnOn(power);
+            Assert.That(!_stringWriter.ToString().Contains("" + power));
         }
     }
 }
